56318a40c5828d22bb380dcc6e2115c4814db803
Audrey Lopez <audreyelopez@gmail.com>
2014-09-12 09:55:20 -0500

    Updated comments

--- public/address_book.php
+++ public/address_book.php
@@ -38,42 +38,14 @@ class AddressDataStore {
     }
 }
 
-    // enact function to save csv file (pass in the filename);
-
+// enact function to save csv file (pass in the filename);
 $ads = new AddressDataStore();
 $address_book = $ads->read_from_csv();
 
-
-
-// =============================================================================
-   
-// make this block of code/method into it's own function; 
-// the new function should read into the csv
-// function add_newcontact_row($handle, $row) {
-//     $handle = fopen(FILENAME, 'r');
-//     while (!feof($handle)) {
-//         $row = fgetcsv($handle);
-
-//         if(!empty($row)) {
-//            $address_book[] = ($row);
-//         }
-//     }
-// }    
-
-// make into multidementional array by breaking down each contact into it's own array
- // $address_book = [
- //      ['The White House, 1000 Abraham Lincoln Drive', 'Washington', 
- //      'DC', 10101],
- //      ['Marvel Comics', 'Lois Lane', 'Long Island', 'NY', 10102],
- //      ['Ima D\'best', '100 Awesome Street', 'Great City', 'GS', 10103]
- //  ];
-
- // Look for newly added contacts in POST 
+// Look for newly added contacts in POST 
     if (!empty($_POST)) {
       // If new contact is present, add from POST to "contacts" array
       // make into array
-      // var_dump($_POST);
-
 
         $contact = [
             $_POST['contact_name'],
@@ -82,37 +54,44 @@ $address_book = $ads->read_from_csv();
             $_POST['city'],
             $_POST['state'],
             $_POST['zip']
-            
         ];
 
-        // check names on these, enter correct variable names - are correct (doublecheck?);
         $address_book[] = $contact;
         $ads->save_to_csv($address_book);
     }  // End POST Check 
 
 
     if (isset($_GET['remove'])) {
-        // define the variable user would like to remove next
+        // define the contact/row user would like to remove
         $keytoremove = $_GET['remove'];
-        // remove item selected; specified with $_GET
+
+        // remove contact selected; specified with $_GET
         unset($address_book[$keytoremove]);
+
         // reindex numbers of list with item removed
         $address_book = array_values($address_book);
+
         //save new todo to file with "save_to_file" function
         $ads->save_to_csv($address_book); 
     }
 
-        // Verify that files were uploaded and no errors occured
+
+    // Verify that files were uploaded and no errors occured
     if (count($_FILES) > 0 && $_FILES ['file1']['error'] == 0) {
+
         // create or set destination directory for uploaded files
         $upload_dir = '/vagrant/sites/planner.dev/public/uploads/';
+
         // select fileName from uploaded file by basename
         $fileName = basename($_FILES['file1']['name']);
+
         // save file under original file name and OUR upload directory
         $saved_filename = $upload_dir . $fileName ;
+        
         // move file to temporary location, our upload directory folder
         move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
-   
+        
+        // add constructor
         $newAds = new AddressDataStore($saved_filename);
 
         $upload_items = $newAds->read_from_csv();
@@ -159,7 +138,6 @@ $address_book = $ads->read_from_csv();
         <button value="submit">Submit</button>
     </form>
 
-
     <form method="POST" enctype="multipart/form-data" action="address_book.php">
         <p>
         <label for="file1">File to upload: </label>
@@ -169,6 +147,5 @@ $address_book = $ads->read_from_csv();
         <input type="submit" value="Upload">
         </p>
     </form>
-
 </body>
 </html>
