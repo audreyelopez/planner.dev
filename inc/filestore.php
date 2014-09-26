8d04979af4f99f271d617f9f186887eb79f492cf
Audrey Lopez <audreyelopez@gmail.com>
2014-09-25 22:49:50 -0500

    reusable functions for address_book / todo_list ect

--- inc/filestore.php
+++ inc/filestore.php
@@ -2,15 +2,39 @@
 
  class Filestore {
 
-     public $filename = '';
-
-     function __construct($filename = '')
-     {
+     private $filename = '';
+     private $is_csv = FALSE;
+     
+     function __construct($filename = ''){
          $this->filename = $filename;
-     }
+         // create a substring to check file extention 
+         if(substr($this->filename, -3) == 'csv'){ 
+            $this->is_csv = TRUE;
+         }
+
+     }   
+         
+         public function read() 
+          {
+            if($this->is_csv) {
+            return $this->read_csv();
+            } else {
+              return $this->read_lines();
+              }  
+
+         }
+
+         public function write($array)
+         {
+          if($this->is_csv) {
+            return $this->write_csv($array);
+           } else {
+              return $this->write_lines($array);
+              }    
+         } 
 // =================================
     // * Returns array of lines in $this->filename
-    function read_lines()
+    private function read_lines()
     {
         $items = array();
         if (is_readable($this->filename) && filesize($this->filename) > 0) {
@@ -26,11 +50,11 @@
      /**
       * Writes each element in $array to a new line in $this->filename
       */
-     function write_lines($items)
+     private function write_lines($items)
      {
        $handle = fopen($this->filename, 'w');
-         foreach ($items as $item => $value) {
-           fwrite($handle, $item . PHP_EOL);
+         foreach ($items as $value) {
+           fwrite($handle, $value . PHP_EOL);
          }
 
          fclose($handle);   
@@ -39,7 +63,7 @@
      /**
       * Reads contents of csv $this->filename, returns an array
       */
-     function read_csv()
+     private function read_csv()
      {
         $array = array();
         $handle = fopen($this->filename, 'r');
@@ -61,18 +85,40 @@
       * Writes contents of $array to csv $this->filename
       */
  
-     function write_csv($array) {
+     private function write_csv($array) {
         $handle = fopen($this->filename, 'w');
 
         foreach ($array as $row) 
         {
             fputcsv($handle, $row);
+
         }
 
         fclose($handle);
         return $array;
     }
    
-   }
+   
+
+   // ===============================
+if (isset($_GET['remove'])) {
+        // define the contact/row user would like to remove
+        // $keytoremove = $_GET['remove'];
+// 
+        // remove contact selected; specified with $_GET
+        // unset($address_book[$keytoremove]);
+// 
+        // reindex numbers of list with item removed
+        // $address_book = array_values($address_book);
+// 
+        // save new contact to file with "save_to_file" function
+        // $ads->write_address_book($address_book); 
+ }
+
+public function remove($row) {
+    foreach $contact as $row {
+
+    }
+}
 
 ?>
